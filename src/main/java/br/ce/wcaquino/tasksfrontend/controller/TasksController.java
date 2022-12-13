72e109f3c64a97f729f26e45ffb2d0a4ab4b0cd2
Rafael Amaral de Paula <rafael.paula@sysmap.com.br>
2022-12-13 14:55:03 -0300

    update jenkins file 27

--- src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
@@ -33,8 +33,8 @@ public class TasksController {
 	@GetMapping("")
 	public String index(Model model) {
 		model.addAttribute("todos", getTodos());
-		if(VERSION.startsWith("build"))
-			model.addAttribute("version", VERSION);
+
+		model.addAttribute("version", VERSION);
 		return "index";
 	}
 	
